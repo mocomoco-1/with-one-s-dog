e8ba83f77fe3915916bb07ddb826339d12c92573
mocomoco <mocomoco-1231@outlook.jp>
2025-08-11 11:34:15 +0900

    Fontawesome導入

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("postcss-url")({
+      url: "copy", // フォントを出力先にコピー
+      assetsPath: "assets", // public/assets に出力
+      useHash: true,
+    }),
+    require("autoprefixer"),
+  ],
+};
