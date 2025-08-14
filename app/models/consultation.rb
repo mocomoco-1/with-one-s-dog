33f521ff87dae8b74a3383847d6271c184b671d2
mocomoco <mocomoco-1231@outlook.jp>
2025-08-14 22:36:01 +0900

    お悩み相談一覧作成

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,6 +1,6 @@
 class Consultation < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
-  validated :content, presence: ture, length: { maximum: 65_535 }
+  validates :content, presence: true, length: { maximum: 65_535 }
 
   belongs_to :user
 end
