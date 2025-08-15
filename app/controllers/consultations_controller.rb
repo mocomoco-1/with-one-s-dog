42809dc9af83ef2d5a3eb71da4988b3f84204d98
mocomoco <mocomoco-1231@outlook.jp>
2025-08-15 11:40:57 +0900

    お悩み相談新規作成機能追加

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,6 +1,26 @@
 class ConsultationsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
   def index
-    @consultations = Consultation.includes(:user)
+    @consultations = Consultation.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @consultation = Consultation.new
+  end
+
+  def create
+    @consultation = current_user.consultations.build(consultation_params)
+    if @consultation.save
+      redirect_to consultations_path, notice: t('defaults.flash_message.created', item: Consultation.model_name.human)
+    else
+      flash.now[:alert] = t('defaults.flash_message.not_created', item: Consultation.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def consultation_params
+    params.require(:consultation).permit(:title, :content)
   end
 end
