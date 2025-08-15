a3a337c9e59924b64cde9c35eb297f413a73a089
mocomoco <mocomoco-1231@outlook.jp>
2025-08-15 09:51:26 +0900

    トップ画面、サイドメニューのデザイン修正

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,5 +1,5 @@
 class ConsultationsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
     @consultations = Consultation.includes(:user)
   end
