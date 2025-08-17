49e0942d8a662e8764661041a67f31ca2fdd3ecf
mocomoco <mocomoco-1231@outlook.jp>
2025-08-17 12:27:11 +0900

    お悩み相談：編集、削除機能追加

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -11,7 +11,7 @@ class ConsultationsController < ApplicationController
   def create
     @consultation = current_user.consultations.build(consultation_params)
     if @consultation.save
-      redirect_to consultations_path, notice: t("defaults.flash_message.created", item: Consultation.model_name.human)
+      redirect_to consultation_path(@consultation), notice: t("defaults.flash_message.created", item: Consultation.model_name.human)
     else
       flash.now[:alert] = t("defaults.flash_message.not_created", item: Consultation.model_name.human)
       render :new, status: :unprocessable_entity
@@ -22,6 +22,26 @@ class ConsultationsController < ApplicationController
     @consultation = Consultation.find(params[:id])
     @comment = Comment.new
     @comments = @consultation.comments.includes(:user).order(created_at: :desc)
+  end
+
+  def edit
+    @consultation = current_user.consultations.find(params[:id])
+  end
+
+  def update
+    @consultation = current_user.consultations.find(params[:id])
+    if @consultation.update(consultation_params)
+      redirect_to consultation_path(@consultation), notice: t("defaults.flash_message.updated", item: Consultation.model_name.human)
+    else
+      flash.now[:alert] = t("defaults.flash_message.mot_updated", item: Consultation.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    consultation = current_user.consultations.find(params[:id])
+    consultation.destroy!
+    redirect_to consultations_path, notice: t("defaults.flash_message.deleted", item: Consultation.model_name.human)
   end
 
   private
