264e9d63c9137eb01487983dc6d0a33ef3bd1505
mocomoco <mocomoco-1231@outlook.jp>
2025-08-22 14:39:37 +0900

    修正③

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,14 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
+rm -rf public/assets/*
 
 # アセットをプリコンパイル（起動時に master.key が読み込まれる）
 RAILS_ENV=production bundle exec rails assets:precompile
 
 RAILS_ENV=production bundle exec rails db:prepare
+
+exec bundle exec rails server -b 0.0.0.0
 
 # # PostgreSQLが完全に起動するまで待機（改良版）
 # echo "Waiting for PostgreSQL to be ready..."
