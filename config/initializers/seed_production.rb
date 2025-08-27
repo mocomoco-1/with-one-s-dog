08bb15f1e66aac3460f258ac7b620dfffcafc323
mocomoco <mocomoco-1231@outlook.jp>
2025-08-27 12:16:14 +0900

    ビルド時読み込まれないよう設定

--- config/initializers/seed_production.rb
+++ config/initializers/seed_production.rb
@@ -1,4 +1,6 @@
-if Rails.env.production? && Question.count == 0
-  Rails.logger.info "💡 本番DBにSeedデータを作成します"
-  load Rails.root.join("db/seeds/diagnosis.rb")
+if Rails.env.production? && defined?(Question)
+  if Question.count == 0
+    Rails.logger.info "💡 本番DBにSeedデータを作成します"
+    load Rails.root.join("db/seeds/diagnosis.rb")
+  end
 end
