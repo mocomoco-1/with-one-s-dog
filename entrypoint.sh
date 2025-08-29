cfd2586a8068dc07592b5cc3f22f6fa3349cd7e2
mocomoco <mocomoco-1231@outlook.jp>
2025-08-29 09:40:24 +0900

    Dockerファイル修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
+# 本番環境の場合のみアセットプリコンパイル
+if [ "$RAILS_ENV" = "production" ]; then
+  echo "Precompiling assets for production..."
+  bundle exec rails assets:clean
+  bundle exec rails assets:precompile
+fi
 # データベース準備
 bundle exec rails db:prepare
 # サーバー起動
