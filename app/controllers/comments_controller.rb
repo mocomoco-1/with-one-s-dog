ab69ec010da9ea0da0b67341ba41072e7476772a
mocomoco <mocomoco-1231@outlook.jp>
2025-08-29 15:12:40 +0900

    お悩み相談:コメント投稿、削除機能のTurbo化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to consultation_path(comment.consultation), success: t("defaults.flash_message.created", item: Comment.model_name.human)
-    else
-      redirect_to consultation_path(comment.consultation), danger: t("defaults.flash_message.not_created", item: Comment.model_name.human)
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
