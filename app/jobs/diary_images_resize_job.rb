9233e33311f7b3e5d4f19706ff4957f7739179ee
mocomoco <mocomoco-1231@outlook.jp>
2025-09-04 21:50:23 +0900

    日記：画像リサイズをバックグラウンド処理に変更

---
+++ app/jobs/diary_images_resize_job.rb
@@ -0,0 +1,10 @@
+class DiaryImagesResizeJob < ApplicationJob
+  queue_as :default
+
+  def perform(diary)
+    diary.images.each do |image|
+      image.variant(resize_to_limit: [350,350]).processed
+      image.variant(resize_to_limit: [100,100]).processed
+    end
+  end
+end
---
+++ test/jobs/diary_images_resize_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DiaryImagesResizeJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
