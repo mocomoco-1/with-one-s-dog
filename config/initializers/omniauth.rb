a8e2d296af43bc057f9a68a8a2fd8a473a9f92eb
mocomoco <mocomoco-1231@outlook.jp>
2025-09-17 14:01:35 +0900

    テスト：LINEログイン9

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-OmniAuth.config.allowed_request_methods = [ :get, :post ]
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :line,
-    ENV["LINE_KEY"],
-    ENV["LINE_SECRET"],
-    scope: "profile openid",
-    provider_ignores_state: true
-end
