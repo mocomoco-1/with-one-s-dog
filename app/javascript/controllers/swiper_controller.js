8108519933026fd618fb378ab8ee8c0e3632d676
mocomoco <mocomoco-1231@outlook.jp>
2025-09-19 22:09:41 +0900

    日記：レスポンシブ変更

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -4,15 +4,17 @@ import { Navigation, Pagination } from 'swiper/modules'
 
 export default class extends Controller {
   connect() {
+    console.log("🌷swiper")
     this.swiper = new Swiper(this.element, {
       modules: [Navigation, Pagination],
       loop: true,
-      slidesPerView: 1,   // 1枚だけ表示する
+      slidesPerView: 1,
       spaceBetween: 10,
       pagination: {
         el: this.element.querySelector(".swiper-pagination"),
         clickable: true,
       },
+      watchOverflow: true,
       navigation: {
         nextEl: this.element.querySelector(".custom-next-btn"),
         prevEl: this.element.querySelector(".custom-prev-btn"),
