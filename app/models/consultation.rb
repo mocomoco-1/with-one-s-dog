b4d51d00ad8dc01133706c05f6766f459ad3b5b3
mocomoco <mocomoco-1231@outlook.jp>
2025-10-09 20:57:24 +0900

    お悩み相談:似た投稿表示を追加

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,5 @@
 class Consultation < ApplicationRecord
+  include PgSearch::Model
   acts_as_taggable_on :tags
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65_535 }
@@ -6,7 +7,12 @@ class Consultation < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :reactions, as: :reactable, dependent: :destroy
-
+  pg_search_scope :search_similar,
+                  against: [ :title, :content ],
+                  using: {
+                    tsearch: { prefix: true }, # 部分一致を可能に
+                    trigram: {} # 文字列類似度も考慮
+                  }
   def self.ransackable_attributes(auth_object = nil)
     %w[title content created_at updated_at]
   end
