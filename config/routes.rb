80a7d08ca18244e5601a2261eb1a2f38e975eba7
mocomoco <mocomoco-1231@outlook.jp>
2025-10-20 10:01:51 +0900

    利用規約、プライバシーポリシー作成

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,8 @@ Rails.application.routes.draw do
   namespace :api do
     get "send_anniversary_notifications/:token", to: "notifications#send_anniversaries"
   end
-
+  get "terms", to: "pages#terms"
+  get "privacy", to: "pages#privacy"
   get "how_to_use", to: "static_pages#how_to_use"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
