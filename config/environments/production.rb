b5322a365cb191ea126aee00ffab28cbe759a624
mocomoco <mocomoco-1231@outlook.jp>
2025-10-24 10:15:57 +0900

    メール送信設定4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,12 +88,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.mailgun.org",
-    port: 587,
+    port: 25,
     domain: ENV["MAILGUN_DOMAIN"],
     user_name: "postmaster@#{ENV["MAILGUN_DOMAIN"]}",
     password: ENV["MAILGUN_API_KEY"],
     authentication: :plain,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    open_timeout: 30,
+    read_timeout: 30,
+    delivery_method: :smtp
   }
 
   config.action_mailer.default_url_options = { host: "tomoni-dogs.com", protocol: "https" }
