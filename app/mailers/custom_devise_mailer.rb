6ab77958f85790f0c44d784a89d081afbb8cdd14
mocomoco <mocomoco-1231@outlook.jp>
2025-10-24 10:05:13 +0900

    メール送信設定3

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,5 +1,4 @@
 class CustomDeviseMailer < Devise::Mailer
-  default from: ENV["MAILGUN_FROM_EMAIL"]
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: "devise/mailer"
