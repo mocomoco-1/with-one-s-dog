37de5083762c34e7f2869fed6c6cbb2f87095a1c
mocomoco <mocomoco-1231@outlook.jp>
2025-10-28 22:45:05 +0900

    LINEログイン修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,23 +9,36 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def basic_action
     @omniauth = request.env["omniauth.auth"]
-    return redirect_to root_path, alert: "認証情報が取得できませんでした" if @omniauth.blank?
-
-    provider = @omniauth["provider"]
-    uid = @omniauth["uid"]
-    info = @omniauth["info"]
-
-    email = info["email"].presence || "#{uid}-#{provider}@example.com"
-
-    @user = User.find_or_create_by!(provider: provider, uid: uid) do |user|
-      user.email = email
-      user.name = info["name"]
-      user.password = Devise.friendly_token[0, 20]
+    if @omniauth.present?
+      @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+      if @profile.email.blank?
+        email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
+        @profile = current_user || User.create!(provider: @omniauth["provider"], uid: @omniauth["uid"], email: email, name: @omniauth["info"]["name"], password: Devise.friendly_token[0, 20])
+      end
+      @profile.set_values(@omniauth)
+      sign_in(:user, @profile)
     end
-    @user.set_values(@omniauth)
-    sign_in(:user, @user)
     flash[:notice] = "ログインしました"
     redirect_to root_path
+
+    # @omniauth = request.env["omniauth.auth"]
+    # return redirect_to root_path, alert: "認証情報が取得できませんでした" if @omniauth.blank?
+
+    # provider = @omniauth["provider"]
+    # uid = @omniauth["uid"]
+    # info = @omniauth["info"]
+
+    # email = info["email"].presence || "#{uid}-#{provider}@example.com"
+
+    # @user = User.find_or_create_by!(provider: provider, uid: uid) do |user|
+    #   user.email = email
+    #   user.name = info["name"]
+    #   user.password = Devise.friendly_token[0, 20]
+    # end
+    # @user.set_values(@omniauth)
+    # sign_in(:user, @user)
+    # flash[:notice] = "ログインしました"
+    # redirect_to root_path
   end
 
   # You should configure your model like this:
