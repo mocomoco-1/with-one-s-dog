22931897a5c1b1ca0d8e5f53324f7987c72532a9
mocomoco <mocomoco-1231@outlook.jp>
2025-10-28 21:43:17 +0900

    Rspec:テスト修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     sequence(:email) { |n| "tes#{n}@example.com" }
     password { "password123" }
     password_confirmation { "password123" }
+    confirmed_at { Time.current }
 
     # LINE認証用（omniauthable対応）
     trait :with_line_auth do
