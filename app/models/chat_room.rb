a120ae3842fff4bfdbd88aac2223e0e0c5cde21e
mocomoco <mocomoco-1231@outlook.jp>
2025-11-10 22:00:11 +0900

    チャット:不要なコード削除

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -2,13 +2,15 @@ class ChatRoom < ApplicationRecord
   has_many :chat_room_users, dependent: :destroy
   has_many :users, through: :chat_room_users
   has_many :chat_messages, dependent: :destroy
+
   # 指定した2人だけが参加している1対1チャットルームを探す
   scope :between, ->(user1_id, user2_id) {
     joins(:chat_room_users)
-    .group("chat_rooms.id")
-    .having("ARRAY_AGG(chat_room_users.user_id) @> ARRAY[?]::bigint[] AND ARRAY_AGG(chat_room_users.user_id) @> ARRAY[?]::bigint[]", user1_id, user2_id)
-    }
-  # roomの名前表示もし名前があればそれ、2人のuserでチャットするなら自分じゃない方のuserの名前を
+      .where(chat_room_users: { user_id: [ user1_id, user2_id ] })
+      .group("chat_rooms.id")
+      .having("COUNT(DISTINCT chat_room_users.user_id) = 2")
+  }
+
   def display_name(user)
     other_user = users.where.not(id: user.id).first
     other_user&.name || "Unknown User"
